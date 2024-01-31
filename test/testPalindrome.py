cdcaa78520f726e61e07bb313fc6f66601b56447
Wexop <mattheurtin@gmail.com>
2024-01-31 15:41:08 +0100

    etape 1 test mirroir [RED]

---
+++ test/testPalindrome.py
@@ -0,0 +1,14 @@
+import unittest
+
+testNonPalindrome = ["maison", "truc", "ceiling-shot", "flip-rest"]
+
+
+class TestPalindrome(unittest.TestCase):
+    def testMirroir(self):
+        for mot in testNonPalindrome:
+            with(self.subTest(mot)):
+                detecteur = DetecteurPalindromme()
+                resultat = detecteur.detecter(mot)
+
+                attendu = mot[::-1]
+                self.assertEquals(attendu, resultat)
